2772c4b7aee0339400f03e529244c1a18c817da7
Julien Blin <julien.blin@gmail.com>
2011-01-02 18:41:07 -0500

    Added ColomboConfigurationSection.

---
+++ src/Colombo/Configuration/EndPointConfigurationElement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Colombo.Configuration
+{
+    public class EndPointConfigurationElement : ConfigurationElement
+    {
+        [ConfigurationProperty("type", IsRequired = true)]
+        public string EndPointType
+        {
+            get
+            {
+                return (string)this["type"];
+            }
+            set
+            {
+                this["type"] = value;
+            }
+        }
+
+        [ConfigurationProperty("assembly", IsRequired = true)]
+        public string AssemblyName
+        {
+            get
+            {
+                return (string)this["assembly"];
+            }
+            set
+            {
+                this["assembly"] = value;
+            }
+        }
+
+        [ConfigurationProperty("address", IsRequired = true)]
+        public string Address
+        {
+            get
+            {
+                return (string)this["address"];
+            }
+            set
+            {
+                this["address"] = value;
+            }
+        }
+    }
+}
---
+++ src/Colombo/Configuration/EndPointsConfigurationCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Diagnostics.Contracts;
+
+namespace Colombo.Configuration
+{
+    [ConfigurationCollection(typeof(EndPointConfigurationElement), AddItemName = "endpoint", CollectionType = ConfigurationElementCollectionType.BasicMap)]
+    public class EndPointsConfigurationCollection : ConfigurationElementCollection
+    {
+        public override ConfigurationElementCollectionType CollectionType
+        {
+            get { return ConfigurationElementCollectionType.BasicMap; }
+        }
+
+        protected override string ElementName
+        {
+            get { return "endpoint"; }
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new EndPointConfigurationElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            var configElement = element as EndPointConfigurationElement;
+            if(configElement == null)
+                throw new ColomboException("Internal error: element should not be null");
+
+            Contract.Assume(configElement.AssemblyName != null);
+            return configElement.AssemblyName;
+        }
+    }
+}
