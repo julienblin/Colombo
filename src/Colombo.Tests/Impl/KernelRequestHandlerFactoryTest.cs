b9fa6d4fca8f43c7e0676ba5c6c6db442a4c390c
Julien Blin <julien.blin@gmail.com>
2011-01-05 17:32:10 -0500

    Added KernelRequestHandlerFactory.

---
+++ src/Colombo.Tests/Impl/KernelRequestHandlerFactoryTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Colombo.Impl;
+using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
+
+namespace Colombo.Tests.Impl
+{
+    [TestFixture]
+    public class KernelRequestHandlerFactoryTest : BaseTest
+    {
+        [Test]
+        public void It_should_ensure_that_it_has_a_IKernel()
+        {
+            Assert.That(() => new KernelRequestHandlerFactory(null),
+                Throws.Exception.TypeOf<ArgumentNullException>()
+                .With.Message.Contains("kernel"));
+        }
+
+        [Test]
+        public void It_should_respond_to_cancreaterequesthandlerfor_depending_on_the_kernel()
+        {
+            var mocks = new MockRepository();
+            var requestHandler1 = mocks.Stub<IRequestHandler<Request1, TestResponse>>();
+
+            var kernel = new DefaultKernel();
+            kernel.Register(
+                Component.For<IRequestHandler<Request1, TestResponse>>().Instance(requestHandler1)
+            );
+
+            var factory = new KernelRequestHandlerFactory(kernel);
+            factory.Logger = GetConsoleLogger();
+            Assert.That(() => factory.CanCreateRequestHandlerFor(new Request1()),
+                Is.True);
+            Assert.That(() => factory.CanCreateRequestHandlerFor(new Request2()),
+                Is.False);
+        }
+
+        public class Request1 : Request<TestResponse>
+        {
+        }
+
+        public class Request2 : Request<TestResponse2>
+        {
+        }
+
+        public class TestResponse2 : Response
+        {
+        }
+
+        [Test]
+        public void It_should_use_the_kernel_to_create_IRequestHandlers()
+        {
+            var mocks = new MockRepository();
+            var requestHandler1 = mocks.Stub<IRequestHandler<Request1, TestResponse>>();
+
+            var kernel = new DefaultKernel();
+            kernel.Register(
+                Component.For<IRequestHandler<Request1, TestResponse>>().Instance(requestHandler1)
+            );
+
+            var factory = new KernelRequestHandlerFactory(kernel);
+            factory.Logger = GetConsoleLogger();
+            Assert.That(() => factory.CreateRequestHandlerFor(new Request1()),
+                Is.SameAs(requestHandler1));
+        }
+
+        [Test]
+        public void It_should_return_null_when_no_IRequestHandlers_can_be_created()
+        {
+            var mocks = new MockRepository();
+
+            var kernel = new DefaultKernel();
+
+            var factory = new KernelRequestHandlerFactory(kernel);
+            factory.Logger = GetConsoleLogger();
+            Assert.That(() => factory.CreateRequestHandlerFor(new Request1()),
+                Is.Null);
+        }
+    }
+}
---
+++ src/Colombo/Impl/KernelRequestHandlerFactory.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.Core.Logging;
+using Castle.MicroKernel;
+using System.Diagnostics.Contracts;
+
+namespace Colombo.Impl
+{
+    /// <summary>
+    /// Implementation of <see cref="IRequestHandlerFactory"/> that uses <see cref="Castle.MicroKernel.IKernel"/>.
+    /// </summary>
+    public class KernelRequestHandlerFactory : IRequestHandlerFactory
+    {
+        private ILogger logger = NullLogger.Instance;
+        public ILogger Logger
+        {
+            get { return logger; }
+            set { logger = value; }
+        }
+
+        private readonly IKernel kernel;
+
+        public KernelRequestHandlerFactory(IKernel kernel)
+        {
+            if (kernel == null) throw new ArgumentNullException("kernel");
+            Contract.EndContractBlock();
+
+            this.kernel = kernel;
+        }
+
+        public bool CanCreateRequestHandlerFor(BaseRequest request)
+        {
+            if (request == null) throw new ArgumentNullException("request");
+            Contract.EndContractBlock();
+
+            var requestHandlerType = CreateIRequestHandlerTypeFrom(request);
+            return kernel.HasComponent(requestHandlerType);
+        }
+
+        public IRequestHandler CreateRequestHandlerFor(BaseRequest request)
+        {
+            if (request == null) throw new ArgumentNullException("request");
+            Contract.EndContractBlock();
+
+            var requestHandlerType = CreateIRequestHandlerTypeFrom(request);
+            try
+            {
+                return (IRequestHandler)kernel.Resolve(requestHandlerType);
+            }
+            catch (ComponentNotFoundException ex)
+            {
+                Logger.ErrorFormat(ex, "ComponentNotFoundException raised for {0} - Should not be the case!", requestHandlerType);
+                return null;
+            }
+        }
+
+        public void DisposeRequestHandler(IRequestHandler requestHandler)
+        {
+            if (requestHandler == null) throw new ArgumentNullException("requestHandler");
+            Contract.EndContractBlock();
+
+            kernel.ReleaseComponent(requestHandler);
+        }
+
+        private static Type CreateIRequestHandlerTypeFrom(BaseRequest request)
+        {
+            if (request == null) throw new ArgumentNullException("request");
+            Contract.EndContractBlock();
+            Contract.Assume(typeof(IRequestHandler<,>).IsGenericTypeDefinition);
+            Contract.Assume(typeof(IRequestHandler<,>).GetGenericArguments().Length == 2);
+
+            Type responseType = request.GetResponseType();
+            Type requestType = request.GetType();
+
+            var requestHandlerType = typeof(IRequestHandler<,>).MakeGenericType(requestType, responseType);
+            return requestHandlerType;
+        }
+    }
+}
