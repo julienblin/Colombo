488c4fcd6a71084c5dfb6e65dcf1bfcc2bda74f2
Julien Blin <julien.blin@gmail.com>
2011-01-06 08:28:58 -0500

    Added WcfClientRequestProcessor.

---
+++ src/Colombo.Tests/Wcf/WcfClientRequestProcessorTest.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Colombo.Wcf;
+using System.ServiceModel;
+
+namespace Colombo.Tests.Wcf
+{
+    [TestFixture]
+    public class WcfClientRequestProcessorTest : BaseTest
+    {
+        [Test]
+        public void It_should_check_WCF_client_configuration_to_determine_if_it_can_process()
+        {
+            var mocks = new MockRepository();
+
+            var processor = new WcfClientRequestProcessor();
+            processor.Logger = GetConsoleLogger();
+            var requestInThisAssembly = new TestRequest();
+            var requestInDynamicAssembly = mocks.Stub<Request<TestResponse>>();
+
+            Assert.That(() => processor.CanProcess(requestInThisAssembly),
+                Is.True);
+            Assert.That(() => processor.CanProcess(requestInDynamicAssembly),
+                Is.False);
+        }
+
+        [Test]
+        public void It_should_create_a_ClientBase_from_configuration()
+        {
+            var processor = new WcfClientRequestProcessor();
+            processor.Logger = GetConsoleLogger();
+
+            var clientBase = processor.CreateClientBase("Colombo.Tests");
+
+            Assert.That(() => clientBase.Endpoint.Address.Uri,
+                Is.EqualTo(new Uri(@"http://localhost/Colombo.svc")));
+        }
+
+        [Test]
+        public void It_should_use_WCF_to_process_requests()
+        {
+            // Create named-pipe services host for testing...
+            using (ServiceHost serviceHost1 = new ServiceHost(typeof(TestWcfService1), new Uri("net.pipe://localhost/ipctest1")))
+            using (ServiceHost serviceHost2 = new ServiceHost(typeof(TestWcfService2), new Uri("net.pipe://localhost/ipctest2")))
+            {
+                serviceHost1.Open();
+                serviceHost2.Open();
+
+                var processor = new WcfClientRequestProcessor();
+                processor.Logger = GetConsoleLogger();
+
+                var request1 = new TestRequestIPC1 { Name = @"Request1" };
+                var request2 = new TestRequestIPC2 { Name = @"Request2" };
+                var request3 = new TestRequestIPC1 { Name = @"Request3" };
+                var request4 = new TestRequestIPC2 { Name = @"Request4" };
+                var requests = new List<BaseRequest> { request1, request2, request3, request4 };
+
+                var responses = processor.Process(requests);
+
+                Assert.That(responses.GetFrom(request1).Name,
+                    Is.EqualTo(request1.Name));
+                Assert.That(responses.GetFrom(request2).Name,
+                    Is.EqualTo(request2.Name));
+                Assert.That(responses.GetFrom(request3).Name,
+                    Is.EqualTo(request3.Name));
+                Assert.That(responses.GetFrom(request4).Name,
+                    Is.EqualTo(request4.Name));
+            }
+        }
+
+        public class TestRequest : Request<TestResponse>
+        {
+        }
+
+        public class TestResponseIpc : Response
+        {
+            public string Name { get; set; }
+        }
+
+        public class TestRequestIPC1 : SideEffectFreeRequest<TestResponseIpc>
+        {
+            public string Name { get; set; }
+
+            public override string GetGroupName()
+            {
+                return "ipctest1";
+            }
+        }
+
+        public class TestWcfService1 : IWcfService
+        {
+            public Response[] Process(BaseRequest[] requests)
+            {
+                return new Response[] { 
+                    new TestResponseIpc { Name = ((TestRequestIPC1)requests[0]).Name },
+                    new TestResponseIpc { Name = ((TestRequestIPC1)requests[1]).Name }
+                };
+            }
+        }
+
+        public class TestRequestIPC2 : SideEffectFreeRequest<TestResponseIpc>
+        {
+            public string Name { get; set; }
+
+            public override string GetGroupName()
+            {
+                return "ipctest2";
+            }
+        }
+
+        public class TestWcfService2 : IWcfService
+        {
+            public Response[] Process(BaseRequest[] requests)
+            {
+                return new Response[] { 
+                    new TestResponseIpc { Name = ((TestRequestIPC2)requests[0]).Name },
+                    new TestResponseIpc { Name = ((TestRequestIPC2)requests[1]).Name }
+                };
+            }
+        }
+
+    }
+}
---
+++ src/Colombo/Wcf/WcfClientRequestProcessor.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.Core.Logging;
+using System.Diagnostics.Contracts;
+using System.ServiceModel.Configuration;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace Colombo.Wcf
+{
+    /// <summary>
+    /// <see cref="IRequestProcessor"/> implementation that uses WCF to transfer requests. See <see cref="IWcfService"/>.
+    /// </summary>
+    public class WcfClientRequestProcessor : IRequestProcessor
+    {
+        private ILogger logger = NullLogger.Instance;
+        public ILogger Logger
+        {
+            get { return logger; }
+            set { logger = value; }
+        }
+
+        public bool CanProcess(BaseRequest request)
+        {
+            if (request == null) throw new ArgumentNullException("request");
+            Contract.EndContractBlock();
+
+            var channelEndpoint = GetChannelEndpointElement(request.GetGroupName());
+            return channelEndpoint != null;
+        }
+
+        public ResponsesGroup Process(IList<BaseRequest> requests)
+        {
+            if (requests == null) throw new ArgumentNullException("requests");
+            Contract.EndContractBlock();
+
+            var requestsGroups = requests.GroupBy(x => x.GetGroupName());
+            foreach (var requestsGroup in requestsGroups)
+            {
+                if (GetChannelEndpointElement(requestsGroup.Key) == null)
+                    throw new ColomboException(string.Format("Internal error: Unable to send to {0}.", requestsGroup.Key));
+            }
+
+            if (Logger.IsDebugEnabled)
+            {
+                Logger.Debug("Mapping for requests/uri is the following:");
+                foreach (var requestGroup in requestsGroups)
+                {
+                    Logger.DebugFormat("{0} => {{", GetChannelEndpointElement(requestGroup.Key).Address.AbsoluteUri);
+                    foreach (var request in requestGroup)
+                    {
+                        Logger.DebugFormat("  {0}", request);
+                    }
+                    Logger.Debug("}");
+                }
+            }
+
+            var tasks = new List<Task<Response[]>>();
+            var tasksGroupAssociation = new Dictionary<string, Task<Response[]>>();
+            foreach (var requestGroup in requestsGroups)
+            {
+                var task = Task.Factory.StartNew<Response[]>((g) =>
+                    {
+                        var group = (IGrouping<string, BaseRequest>)g;
+                        var clientBase = CreateClientBase(group.Key);
+
+                        Logger.DebugFormat("Sending {0} request(s) to {1}...", group.Count(), clientBase.Endpoint.Address.Uri);
+                        return clientBase.Process(group.ToArray());
+                    },
+                    requestGroup
+                );
+                tasks.Add(task);
+                tasksGroupAssociation[requestGroup.Key] = task;
+            }
+            try
+            {
+                Task.WaitAll(tasks.ToArray());
+            }
+            catch (AggregateException ex)
+            {
+                string message = "An exception occured inside one or several WCF endpoint";
+                Logger.Error(message, ex);
+                throw new ColomboException(message, ex);
+            }
+
+            Logger.Debug("All the WCF clients have executed successfully.");
+
+            Logger.Debug("Reconstituing responses...");
+            var responses = new ResponsesGroup();
+            foreach (var requestsGroup in requestsGroups)
+            {
+                var task = tasksGroupAssociation[requestsGroup.Key];
+                var requestsArray = requestsGroup.ToArray();
+                for (int i = 0; i < requestsArray.Length; i++)
+                {
+                    var request = requestsArray[i];
+                    responses[request] = task.Result[i];
+                }
+            }
+
+            Logger.DebugFormat("{0} responses are returned.", responses.Count);
+
+            Contract.Assume(responses.Count == requests.Count);
+            return responses;
+
+            throw new NotImplementedException();
+        }
+
+        public WcfClientBaseService CreateClientBase(string groupName)
+        {
+            Contract.Assume(groupName != null);
+            try
+            {
+                return new WcfClientBaseService(groupName);
+            }
+            catch (InvalidOperationException ex)
+            {
+                var errorMessage = string.Format("Unable to create a WCF ClientBase. Did you create a WCF client endPoint with the name {0}?", groupName);
+                Logger.Error(errorMessage, ex);
+                throw new ColomboException(errorMessage, ex);
+            }
+        }
+
+        public ChannelEndpointElement GetChannelEndpointElement(string endPointName)
+        {
+            if (WcfConfigClientSection == null)
+                return null;
+
+            foreach (ChannelEndpointElement endPoint in WcfConfigClientSection.Endpoints)
+            {
+                if (endPoint.Name.Equals(endPointName, StringComparison.InvariantCultureIgnoreCase))
+                    return endPoint;
+            }
+
+            return null;
+        }
+
+        private ClientSection clientSection = null;
+
+        public ClientSection WcfConfigClientSection
+        {
+            get
+            {
+                if (clientSection == null)
+                {
+                    Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                    ServiceModelSectionGroup serviceModelGroup = ServiceModelSectionGroup.GetSectionGroup(configuration);
+                    if (serviceModelGroup != null)
+                        clientSection = serviceModelGroup.Client;
+                }
+                return clientSection;
+            }
+        }
+    }
+}
