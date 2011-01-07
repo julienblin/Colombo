20c4c530c9f221bb66aa0873675f56e4cc6d2d0a
Julien Blin <julien.blin@gmail.com>
2011-01-07 15:13:51 -0500

    Added missing contracts.

---
+++ src/Colombo/Contracts/MessageBusSendInterceptorContract.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+
+namespace Colombo.Contracts
+{
+    [ContractClassFor(typeof(IMessageBusSendInterceptor))]
+    public abstract class MessageBusSendInterceptorContract : IMessageBusSendInterceptor
+    {
+        public void Intercept(IColomboSendInvocation nextInvocation)
+        {
+            Contract.Requires<ArgumentNullException>(nextInvocation != null, "nextInvocation");
+            throw new NotImplementedException();
+        }
+
+        public int InterceptionPriority
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
--- src/Colombo/IMessageBusSendInterceptor.cs
+++ src/Colombo/IMessageBusSendInterceptor.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics.Contracts;
 
 namespace Colombo
 {
     /// <summary>
     /// Interceptor for the <see cref="IMessageBus.Send"/> operation.
     /// </summary>
+    [ContractClass(typeof(Contracts.MessageBusSendInterceptorContract))]
     public interface IMessageBusSendInterceptor : IColomboInterceptor
     {
         /// <summary>
