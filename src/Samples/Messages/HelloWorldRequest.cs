d1a885f587820688cff4ec568cfe86f45a22fdbb
Julien Blin <julien.blin@gmail.com>
2011-01-07 15:41:50 -0500

    Added Samples.

---
+++ src/Samples/Messages/HelloWorldRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using Colombo.Interceptors;
+
+namespace Colombo.Samples.Messages
+{
+    [SLA(500)]
+    [RequiredInContext(CurrentCultureConstant.CultureContextKey)]
+    public class HelloWorldRequest : Request<HelloWorldResponse>
+    {
+        [Required]
+        [StringLength(10)]
+        public string Name { get; set; }
+    }
+}
---
+++ src/Samples/WcfService/Handlers/HelloWorldHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Colombo.Samples.Messages;
+using System.Threading;
+using System.Resources;
+using Colombo.Samples.WcfService.L10n;
+
+namespace Colombo.Samples.WcfService.Handlers
+{
+    public class HelloWorldHandler : RequestHandler<HelloWorldRequest, HelloWorldResponse>
+    {
+        public override void Handle()
+        {
+            Response.Message = string.Format(Resources.Hello, Request.Name);
+        }
+    }
+}
