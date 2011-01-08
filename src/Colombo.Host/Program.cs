b182850fdbdc3a18720b272073db2932c94fa97b
Julien Blin <julien.blin@gmail.com>
2011-01-08 17:29:49 -0500

    Added Topshelf as lib.

--- src/Colombo.Host/Program.cs
+++ src/Colombo.Host/Program.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
 namespace Colombo.Host
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
