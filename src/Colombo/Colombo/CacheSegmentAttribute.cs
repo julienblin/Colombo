3451df022e0ef69654f914aed22b67340ff287b5
Julien Blin <julien.blin@gmail.com>
2011-01-10 14:52:57 -0500

    Added Caching attributes for client-side.

---
+++ src/Colombo/Colombo/CacheSegmentAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Colombo
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class CacheSegmentAttribute : Attribute
+    {
+        public string Name { get; set; }
+        public string FromContextKey { get; set; }
+
+        public CacheSegmentAttribute()
+        {
+        }
+
+        public CacheSegmentAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string GetCacheSegment(BaseRequest request)
+        {
+            if (string.IsNullOrEmpty(Name) && string.IsNullOrEmpty(FromContextKey))
+                throw new ColomboException(string.Format("Unable to determine cache segment for {0}. You must specified either the Name or the FromContextKey member."));
+
+            if (!string.IsNullOrEmpty(FromContextKey))
+            {
+                if (request.Context.ContainsKey(FromContextKey))
+                    return request.Context[FromContextKey];
+
+                if (string.IsNullOrEmpty(Name))
+                    throw new ColomboException(string.Format("The cache segment for {0} is supposed to come from the context key {1}, but it doesn't exists on Context and no Name has been given.",
+                        request,
+                        FromContextKey));
+            }
+
+            return Name;
+        }
+    }
+}
---
+++ src/Colombo/Colombo/EnableClientCachingAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Colombo
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple=false, Inherited=false)]
+    public class EnableClientCachingAttribute : Attribute
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        public TimeSpan Expiration
+        {
+            get
+            {
+                if (Hours == 0 && Minutes == 0 && Seconds == 0)
+                {
+                    throw new ColomboException("You need to specify at least an hour value, a minute value or a second value.");
+                }
+
+                return new TimeSpan(0, Hours, Minutes, Seconds);
+            }
+        }
+    }
+}
