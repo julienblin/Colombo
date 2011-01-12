3052c9763ccaeabecc612c1ce922c518bbc2bb09
Julien Blin <julien.blin@gmail.com>
2011-01-12 11:23:41 -0500

    Added KernelNotificationHandlerFactory.

---
+++ src/Colombo/Colombo.Tests/Impl/KernelNotificationHandlerFactoryTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Colombo.Impl;
+using Rhino.Mocks;
+using Castle.MicroKernel;
+using Castle.MicroKernel.Registration;
+
+namespace Colombo.Tests.Impl
+{
+    [TestFixture]
+    public class KernelNotificationHandlerFactoryTest : BaseTest
+    {
+        [Test]
+        public void It_should_ensure_that_it_has_a_IKernel()
+        {
+            Assert.That(() => new KernelNotificationHandlerFactory(null),
+                Throws.Exception.TypeOf<ArgumentNullException>()
+                .With.Message.Contains("kernel"));
+        }
+
+        [Test]
+        public void It_should_respond_to_cancreaterequesthandlerfor_depending_on_the_kernel()
+        {
+            var mocks = new MockRepository();
+            var notificationHandler = mocks.Stub<INotificationHandler<TestNotification>>();
+
+            var kernel = new DefaultKernel();
+            kernel.Register(
+                Component.For<INotificationHandler<TestNotification>>().Instance(notificationHandler)
+            );
+
+            var factory = new KernelNotificationHandlerFactory(kernel);
+            factory.Logger = GetConsoleLogger();
+            Assert.That(() => factory.CanCreateNotificationHandlerFor(new TestNotification()),
+                Is.True);
+            Assert.That(() => factory.CanCreateNotificationHandlerFor(new TestNotification2()),
+                Is.False);
+        }
+
+        [Test]
+        public void It_should_use_the_kernel_to_create_INotificationHandlers()
+        {
+            var kernel = new DefaultKernel();
+            kernel.Register(
+                Component.For<INotificationHandler<TestNotification>>()
+                    .LifeStyle.Transient
+                    .ImplementedBy<TestNotificationHandler>(),
+                Component.For<INotificationHandler<TestNotification>>()
+                    .LifeStyle.Transient
+                    .ImplementedBy<TestNotificationHandler2>()
+            );
+
+            var factory = new KernelNotificationHandlerFactory(kernel);
+            factory.Logger = GetConsoleLogger();
+            var notificationhandlers = factory.CreateNotificationHandlersFor(new TestNotification());
+            Assert.That(() => notificationhandlers.Length,
+                Is.EqualTo(2));
+            Assert.That(() => notificationhandlers[0],
+                Is.TypeOf<TestNotificationHandler>());
+            Assert.That(() => notificationhandlers[1],
+                Is.TypeOf<TestNotificationHandler2>());
+
+            notificationhandlers = factory.CreateNotificationHandlersFor(new TestNotification2());
+            Assert.That(() => notificationhandlers.Length,
+                Is.EqualTo(0));
+        }
+
+        public class TestNotification : Notification { }
+
+        public class TestNotification2 : Notification { }
+
+        public class TestNotificationHandler : NotificationHandler<TestNotification>
+        {
+            public override void Handle()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class TestNotificationHandler2 : NotificationHandler<TestNotification>
+        {
+            public override void Handle()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ src/Colombo/Colombo/Impl/KernelNotificationHandlerFactory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.Core.Logging;
+using Castle.MicroKernel;
+using System.Diagnostics.Contracts;
+
+namespace Colombo.Impl
+{
+    /// <summary>
+    /// Implementation of <see cref="INotificationHandlerFactory"/> that uses <see cref="Castle.MicroKernel.IKernel"/>.
+    /// </summary>
+    public class KernelNotificationHandlerFactory : INotificationHandlerFactory
+    {
+        private ILogger logger = NullLogger.Instance;
+        public ILogger Logger
+        {
+            get { return logger; }
+            set { logger = value; }
+        }
+
+        private readonly IKernel kernel;
+
+        public KernelNotificationHandlerFactory(IKernel kernel)
+        {
+            if (kernel == null) throw new ArgumentNullException("kernel");
+            Contract.EndContractBlock();
+
+            this.kernel = kernel;
+        }
+
+        public bool CanCreateNotificationHandlerFor(Notification notification)
+        {
+            if (notification == null) throw new ArgumentNullException("reqnotificationuest");
+            Contract.EndContractBlock();
+
+            var requestNotificationType = CreateNotificationHandlerTypeFrom(notification);
+            return kernel.HasComponent(requestNotificationType);
+        }
+
+        public INotificationHandler[] CreateNotificationHandlersFor(Notification notification)
+        {
+            if (notification == null) throw new ArgumentNullException("reqnotificationuest");
+            Contract.EndContractBlock();
+
+            var requestNotificationType = CreateNotificationHandlerTypeFrom(notification);
+            return (INotificationHandler[])kernel.ResolveAll(requestNotificationType);
+        }
+
+        public void DisposeNotificationHandler(INotificationHandler notificationHandler)
+        {
+            if (notificationHandler == null) throw new ArgumentNullException("notificationHandler");
+            Contract.EndContractBlock();
+
+            kernel.ReleaseComponent(notificationHandler);
+        }
+
+        private static Type CreateNotificationHandlerTypeFrom(Notification notification)
+        {
+            if (notification == null) throw new ArgumentNullException("notification");
+            Contract.EndContractBlock();
+
+            Type notificationType = notification.GetType();
+
+            Contract.Assume(typeof(INotificationHandler<>).IsGenericTypeDefinition);
+            Contract.Assume(typeof(INotificationHandler<>).GetGenericArguments().Length == 1);
+            return typeof(INotificationHandler<>).MakeGenericType(notificationType);
+        }
+    }
+}
