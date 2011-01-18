2c1ddfdbd150528cd064a494a2f04a5a8f16ad30
Julien Blin <julien.blin@gmail.com>
2011-01-18 11:08:56 -0500

    Updated sample to include Unit Tests for TestSupport.

---
+++ src/Samples/Hosted.Tests/BaseTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.Windsor;
+using Colombo;
+using Colombo.Facilities;
+using Colombo.TestSupport;
+using NUnit.Framework;
+
+namespace Hosted.Tests
+{
+    public class BaseTest
+    {
+        protected IWindsorContainer Container { get; set; }
+
+        protected IStubMessageBus StubMessageBus { get; set; }
+
+        protected IMessageBus MessageBus { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Container = new WindsorContainer();
+            Container.AddFacility<ColomboFacility>(f => f.TestSupportMode());
+
+            StubMessageBus = Container.Resolve<IStubMessageBus>();
+            MessageBus = Container.Resolve<IMessageBus>();
+        }
+    }
+}
---
+++ src/Samples/Hosted.Tests/HelloWorldHandlerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Colombo.Samples.Hosted.Handlers;
+using Colombo.Samples.Messages;
+using NUnit.Framework;
+
+namespace Hosted.Tests
+{
+    [TestFixture]
+    public class HelloWorldHandlerTest : BaseTest
+    {
+        [Test]
+        public void It_should_respond_using_the_request_Name_and_notify()
+        {
+            var request = new HelloWorldRequest() { Name = "TheName" };
+
+            StubMessageBus.TestHandler<HelloWorldHandler>();
+            StubMessageBus
+                .ExpectNotify<HelloWorldNotification>()
+                .Assert(n => n.Name.Equals(request.Name));
+
+            var response = MessageBus.Send(request);
+
+            Assert.That(response.Message.Contains("TheName"));
+            StubMessageBus.Verify();
+        }
+
+        [Test]
+        public void It_should_reject_Requests_without_Name_and_not_notify()
+        {
+            StubMessageBus.TestHandler<HelloWorldHandler>().ShouldBeInterceptedBeforeHandling();
+
+            var response = MessageBus.Send(new HelloWorldRequest());
+
+            Assert.IsFalse(response.IsValid());
+            StubMessageBus.Verify();
+        }
+    }
+}
